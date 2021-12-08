3239b32561424eba67427e0d56ff35c49f0c4d3f
Crazy <rg703612@gmail.com>
2021-12-08 09:22:29 +0530

    Deleted some files.

--- app/src/main/java/com/example/myproject/AppointmentListActivity.java
+++ app/src/main/java/com/example/myproject/AppointmentListActivity.java
@@ -8,15 +8,11 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.room.Room;
 
-import com.example.myproject.adapters.AppointmentAdapter;
-import com.example.myproject.adapters.LabAdapter;
 import com.example.myproject.database.AppDatabase;
 import com.example.myproject.database.dao.AppointmentDao;
-import com.example.myproject.database.dao.LabDao;
-import com.example.myproject.database.entities.Lab;
+import com.example.myproject.database.entities.Appointment;
 import com.example.myproject.databinding.ActivityAppointmentListBinding;
 import com.example.myproject.utils.SharedPrefManager;
-import com.example.myproject.database.entities.Appointment;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -27,7 +23,6 @@ public class AppointmentListActivity extends AppCompatActivity {
     private Context context = this;
     private SharedPrefManager sharedPrefManager;
     private List<Appointment> appointmentList = new ArrayList<>();
-    private AppointmentAdapter appointmentAdapter;
     AppDatabase db;
 
     @Override
@@ -50,25 +45,10 @@ public class AppointmentListActivity extends AppCompatActivity {
 
 
     private void init() {
-        appointmentAdapter = new AppointmentAdapter(context, appointmentList);
         AppointmentDao dao = db.appointmentDao();
 
-        binding.recyclerView.setAdapter(appointmentAdapter);
 
-        new Thread(() -> {
-            appointmentList.clear();
-            appointmentList.addAll(dao.getAll());
-            runOnUiThread(() -> {
-                appointmentAdapter.notifyDataSetChanged();
-            });
-        }).start();
-
-
-        binding.fab.setOnClickListener(view -> {
-            openActivity(CreateAppointmentActivity.class);
-        });
     }
-
 
 
     private void openActivity(Class aclass) {
--- app/src/main/java/com/example/myproject/LabListActivity.java
+++ app/src/main/java/com/example/myproject/LabListActivity.java
@@ -8,24 +8,17 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.room.Room;
 
-import com.example.myproject.adapters.LabAdapter;
 import com.example.myproject.database.AppDatabase;
 import com.example.myproject.database.dao.LabDao;
-import com.example.myproject.database.entities.Lab;
 import com.example.myproject.databinding.ActivityLabListBinding;
 import com.example.myproject.utils.SharedPrefManager;
 
-import java.util.ArrayList;
-import java.util.List;
-
-public class LabListActivity extends AppCompatActivity implements LabAdapter.LabInterface {
+public class LabListActivity extends AppCompatActivity {
 
     private ActivityLabListBinding binding;
     private Context context = this;
     private SharedPrefManager sharedPrefManager;
-    private List<Lab> labList = new ArrayList<>();
-    private LabAdapter labAdapter;
-    AppDatabase db;
+      AppDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,46 +40,14 @@ public class LabListActivity extends AppCompatActivity implements LabAdapter.Lab
 
 
     private void init() {
-        labAdapter = new LabAdapter(context, labList, this);
-        LabDao labDao = db.labDao();
-
-        binding.recyclerView.setAdapter(labAdapter);
-
-        new Thread(() -> {
-            labList.clear();
-            labList.addAll(labDao.getAll());
-            runOnUiThread(() -> {
-                labAdapter.notifyDataSetChanged();
-            });
-        }).start();
+         LabDao labDao = db.labDao();
 
 
-        binding.fab.setOnClickListener(view -> {
-            openActivity(CreateLabActivity.class);
-        });
+
+
     }
 
 
-    @Override
-    public void onEdit(Lab lab) {
-        Intent intent = new Intent(context, EditLabActivity.class);
-        intent.putExtra("lab_id",lab.labId);
-        startActivity(intent);
-    }
-
-    @Override
-    public void onDelete(Lab lab) {
-        LabDao labDao = db.labDao();
-
-        new Thread(() -> {
-            labDao.deleteLab(lab.labId);
-            labList.clear();
-            labList.addAll(labDao.getAll());
-            runOnUiThread(() -> {
-                labAdapter.notifyDataSetChanged();
-            });
-        }).start();
-    }
 
     private void openActivity(Class aclass) {
         Intent intent = new Intent(context, aclass);
